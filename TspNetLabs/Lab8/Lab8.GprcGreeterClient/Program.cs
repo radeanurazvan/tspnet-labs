02ec61af3f01313c6a04302b5f3050e2614ac3a9
Razvan <radeanu.razvan99@gmail.com>
2020-04-08 16:36:58 +0300

    Implemented lab8

---
+++ TspNetLabs/Lab8/Lab8.GprcGreeterClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Lab8.GprcGreeter;
+
+namespace Lab8.GprcGreeterClient
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "GreeterClient" });
+            Console.WriteLine("Greeting: " + reply.Message);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
